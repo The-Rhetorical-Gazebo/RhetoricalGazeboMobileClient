beb51f0490ffe0988665dd5d8d23a5fc065c832a
TylerMan <tbaughcome@gmail.com>
2022-01-03 16:45:45 -0500

    Init mobile client repo

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import "package:flutter_dotenv/flutter_dotenv.dart";
+import "./employee/auth/choice.dart";
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Choice());
+  }
+}
